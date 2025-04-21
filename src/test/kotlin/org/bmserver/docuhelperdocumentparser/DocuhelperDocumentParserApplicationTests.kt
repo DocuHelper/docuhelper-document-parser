36a766baa87280af194d827b06a7ac669a85f387
BmSrv <jmspon33@gmail.com>
2025-04-21 20:00:07 +0900

    init

---
+++ src/test/kotlin/org/bmserver/docuhelperdocumentparser/DocuhelperDocumentParserApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bmserver.docuhelperdocumentparser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocuhelperDocumentParserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
